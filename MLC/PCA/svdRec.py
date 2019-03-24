dabc1ad6262d7ea94a847ddcbf7a3083b196624f
leviwang <1064792286@qq.com>
2019-03-24 17:54:49 +0800

    update

--- MLC/PCA/svdRec.py
+++ MLC/PCA/svdRec.py
@@ -38,20 +38,7 @@ def cosSim(inA,inB):
 	denom = la.norm(inA)*la.norm(inB)
 	return 0.5+0.5*(num/denom)
 
-#推荐函数
-def recommend(dataMat,user,N=3,simMeas = cosSim,estMethod  = standEst):
-	#获取特定user商品评价为0的列
-	unrateItems = nonzero(dataMat[user,:].A==0)[1]
-	if len(unrateItems) == 0:return 'you rated everything'
-	itemScores = []
-	#对每个评价缺失的商品，调用svdEst或者standEst进行相似度计算
-	for item in unrateItems:
-		#计算获得评分
-		estimatedScore = estMethod(dataMat,user,simMeas,item)
-		#商品，评分对应保存
-		itemScores.append((item,estimatedScore))
-	#返回相识度排名前三的未评价商品以及对应计算出来的评分
-	return sorted(itemScores,key= lambda jj:jj[1],reverse=True)[:N]
+
 
 #通过SVD降维处理相似度函数
 
@@ -104,19 +91,37 @@ def standEst(dataMat,user,simMeas,item):
 	if simTotal ==0:return 0
 	else:return ratSimTotal/simTotal
 
+
+#推荐函数
+def recommend(dataMat,user,N=3,simMeas = cosSim,estMethod  = standEst):
+	#获取特定user商品评价为0的列
+	unrateItems = nonzero(dataMat[user,:].A==0)[1]
+	if len(unrateItems) == 0:return 'you rated everything'
+	itemScores = []
+	#对每个评价缺失的商品，调用svdEst或者standEst进行相似度计算
+	for item in unrateItems:
+		#计算获得评分
+		estimatedScore = estMethod(dataMat,user,simMeas,item)
+		#商品，评分对应保存
+		itemScores.append((item,estimatedScore))
+	#返回相识度排名前三的未评价商品以及对应计算出来的评分
+	return sorted(itemScores,key= lambda jj:jj[1],reverse=True)[:N]
+
 # 使用SVD 来压缩图片
 
+#构建打印矩阵函数
 def printMat(inMat,thresh = 0.8):
-	for i in  range(32):
+	for i in range(32):
 		for k in range(32):
 			if float(inMat[i,k])>thresh:
-				print(1)
-			else:print(0)
-		print(' ')
+				print(1,end="")
+			else:print(0,end="")
+		print('')
 
+#图像像素压缩
 def imgCompress(numSV=3,thresh=0.8):
 	myl = []
-	for line in open('PCA/0_5.txt').readlines():
+	for line in open('MLC/PCA/0_5.txt').readlines():
 		newRow = []
 		for i in range(32):
 			newRow.append(int(line[i]))
@@ -124,13 +129,17 @@ def imgCompress(numSV=3,thresh=0.8):
 	myMat = mat(myl)
 	print("****original matrix****")
 	printMat(myMat,thresh)
+	#先试用SVD分解
 	U,Sigma,VT = la.svd(myMat)
+	#构造3*3 0值矩阵
 	SigRecon = mat(zeros((numSV,numSV)))
 	for k in range(numSV):
 		SigRecon[k,k] = Sigma[k]
+	#利用降维后的矩阵，还原原矩阵，从另一个角度来看，通过分解矩阵达到压缩存储空间的目的
 	reconMat = U[:,:numSV]*SigRecon*VT[:numSV,:]
 	print("****reconstructed matrix using %d singular values******"%numSV)
 	printMat(reconMat,thresh)
+
 
 
 
