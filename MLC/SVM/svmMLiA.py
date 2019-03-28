584f48638f525dcd580631db48b745d038a4e69a
leviwang <1064792286@qq.com>
2019-03-29 07:58:38 +0800

    update

--- MLC/SVM/svmMLiA.py
+++ MLC/SVM/svmMLiA.py
@@ -32,22 +32,31 @@ def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
     while (iter < maxIter):
         alphaPairsChanged = 0
         for i in range(m):
-            #
+            # 求的当前alphas下，第i条数据对应的y值
             fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T))+b
+            # 比较计算值和实际值之间的误差
             Ei = fXi - float(labelMat[i])
+            #根据KTT条件，判断是否满足需要优化的条件
             if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and \
                                                                    (alphas[i] > 0)):
+                #在0和m之间选择j,确保i不等于j
                 j = selectJrand(i,m)
-                fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[j,:].T))+b
+                #在当前alphas值下，计算第j条数据对应y值
+                fXj = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[j,:].T))+b
+                #比较计算值和当前值的误差
                 Ej = fXi -float(labelMat[j])
+                #根据python引用传递的规则，需要重新分配内存保存旧的alphas，从而实现比较新旧alphas的目的
                 alphasIold = alphas[i].copy(); alphasJold = alphas[j].copy();
+                #根据y值是否相等，分别计算不同边界值L，H，用于将alphas调整至0至C之间
                 if (labelMat[i] != labelMat[j]):
                     L = max(0,alphas[j]-alphas[i])
                     H = min(C,C+alphas[j]-alphas[i])
                 else:
                     L = max(0,alphas[j] + alphas[i] -C)
                     H = min(C,alphas[j] + alphas[i])
+                #如果L等于H，则不需要调整alphas，结束本次循环
                 if L==H:print("L=H");continue
+                #计算alphas[j]的最佳修改值
                 eta = 2.0 * dataMatrix[i,:]*dataMatrix[j,:].T - dataMatrix[i,:]*dataMatrix[i,:].T-\
                     dataMatrix[j,:]*dataMatrix[j,:].T
                 if eta >= 0:print("eta>=0");continue
